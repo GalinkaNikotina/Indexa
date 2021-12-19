bcb9f98cec92e5d76ca12b979cc722c7f52692f7
Galinka <galinka2002niko@gmail.com>
2021-12-20 00:09:59 +0300

    qw

---
+++ Index/Index/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Index
+{
+
+    class SampleCollection<T>
+    {
+        // Объявление массива для хранения элементов данных
+        private T[] arr = new T[100];
+
+        // Определение индексатора,
+        // чтобы клиентский код мог использовать нотацию []
+        public T this[int i]
+        {
+            get { return arr[i]; }
+            set { arr[i] = value; }
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            var stringCollection = new SampleCollection<string>();
+            stringCollection[0] = "Hello, World";
+            Console.WriteLine(stringCollection[0]);
+        }
+    }
+}
